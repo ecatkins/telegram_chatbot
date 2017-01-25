e69f1a5fc64f9e948085c21397ddb25139b6bfe1
ecatkins <ecatkins@gmail.com>
2017-01-25 15:57:51 -0500

    Adding some documentation

--- main_chatbot.py
+++ main_chatbot.py
@@ -2,11 +2,8 @@ import json
 import requests
 import time
 import urllib
-
 from gensim import utils, matutils, models
-
 from sklearn.externals import joblib
-
 from secret import *
 
 
@@ -14,17 +11,20 @@ from secret import *
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 def get_url(url):
+    ''' Gets url that returns content '''
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
 
 
 def get_json_from_url(url):
+    '''Gets json from url '''
     content = get_url(url)
     js = json.loads(content)
     return js
 
 def get_updates(offset=None):
+    ''' Gets the updates from the Telegram server '''
     url = URL + "getUpdates?timeout=100"
     if offset:
         url += "&offset={}".format(offset)
@@ -33,12 +33,16 @@ def get_updates(offset=None):
 
 
 def get_last_update_id(updates):
+    ''' Gets the ID of the last update '''
+
     update_ids = []
     for update in updates["result"]:
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
 
 def get_last_chat_id_and_text(updates):
+    ''' Gets the last chat ID and the text '''
+
     num_updates = len(updates["result"])
     last_update = num_updates - 1
     text = updates["result"][last_update]["message"]["text"]
@@ -46,18 +50,21 @@ def get_last_chat_id_and_text(updates):
     return (text, chat_id)
 
 def send_message(text, chat_id):
+    ''' Sends message to client '''
     text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
 
 def echo_all(updates):
+    ''' Tests echoing messages received '''
     for update in updates["result"]:
         text = update["message"]["text"]
         chat = update["message"]["chat"]["id"]
         send_message(text, chat)
 
 def basic_responses(updates):
+    ''' Tests basic rule-set for sending messages '''
     for update in updates["result"]:
         chat = update["message"]["chat"]["id"]
         text = update["message"]["text"]
@@ -70,14 +77,21 @@ def basic_responses(updates):
         send_message(response, chat)
 
 def doc_response(updates):
+    ''' Generates responses to messages recived '''
 
+    # For each message
     for update in updates["result"]:
+        # Extract chat_id and text
         chat = update["message"]["chat"]["id"]
         text = update["message"]["text"]
+        # Turn input text into a vector and reshape
         vector = doc_model.infer_vector(utils.simple_preprocess(text), steps = 40)
         vector = vector.reshape(1, -1)
+        # Predict the appropriate response label
         prediction = logistic_model.predict(vector)
+        # Use label to exstract the text from the array of all possible responses
         response = possible_responses[prediction[0]]
+        # Send message to client
         send_message(response, chat)
 
 
@@ -85,16 +99,23 @@ def doc_response(updates):
 
 
 def main():
-
+    ''' Runs the chatbot functionality in a loop '''
     last_update_id = None
+    # Loop indefinitel
     while True:
+        # Get the latest messages from client
         updates = get_updates(last_update_id)
+        # If they exist
         if len(updates["result"]) > 0:
+            # Set last_update_id
             last_update_id = get_last_update_id(updates) + 1
+            # Generate response to message
             doc_response(updates)
+        # Sleep program for half a second
         time.sleep(0.5)
 
 if __name__ == '__main__':
+    # Loads saved models from disk
     doc_model = models.Doc2Vec.load(doc2vec_location)
     model_details = joblib.load('model.pkl')
     logistic_model = model_details['model']
--- train_chatbot.py
+++ train_chatbot.py
@@ -1,30 +1,42 @@
 import numpy as np
 import pandas as pd
 import gensim
-
 from gensim import utils, matutils, models
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.externals import joblib
-
 from secret import *
 
 def train_main():
+	'''	Trains an sklearn logistic regresion model on the given 
+		example set, utilising a pre-trained Doc2Vec model to 
+		generate feature vectors for text
+	'''
+
+	# Reads the training examples
 	examples = pd.read_csv('hardcode_examples.csv')
 
+	# Loads the Doc2Vdec model
 	doc_model = models.Doc2Vec.load(doc2vec_location)
 
+	# Initiates empty arrays for variables and labels
 	doc_features = np.empty([0, doc_model.vector_size])
 	labels = []
 
+	# Initiaties a list to store the textual responses for later replaying
 	unique_responses = []
 
+	# For each example
 	for index, row in examples.iterrows():
+		
+		# Extract the text and the ideal response
 		text = row['Text']
 		response = row['Response']
+
+		# Create a feature vector for the text and add to feature array 
 		vector = doc_model.infer_vector(utils.simple_preprocess(text), steps = 40)
 		doc_features = np.concatenate((doc_features, [np.array(vector)]), axis = 0)
 
+		# Add the label to that array and the text of the response to that array
 		if response in unique_responses:
 			label = unique_responses.index(response)
 		else:
@@ -33,12 +45,13 @@ def train_main():
 		labels.append(label)
 
 	labels = np.array(labels)	
+	
+	# Generate and fit logistic regression model
 	model = LogisticRegression()
-
 	model.fit(doc_features, labels)
 
+	# Save to disk
 	model_details = {'model':model, 'response_vector': unique_responses}
-
 	joblib.dump(model_details, 'model.pkl')
 
 
